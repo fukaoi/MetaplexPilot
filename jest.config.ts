5962743a0cdd49d69206b41d15e0063e2c087f77
fukaoi <usapyon2022@gmail.com>
2025-06-16 14:49:18 +0900

    wip

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-  // testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
 };
+
+export default config;
